34a50aa66ceea0489c6964af7385e30173a8b178
Ash Srinivas <asriniv4@its.jnj.com>
2023-01-10 02:09:58 -0800

    Added basic navbar

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,28 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <nav>
-      <RouterLink to="/aurora">Orders List</RouterLink>
-    </nav>
-    <RouterView />
-  </div>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" to="/aurora">Orders List</RouterLink>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">View 3D Model</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Support</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
 </template>
 
 <style>
--- src/components/OrdersList.vue
+++ src/components/OrdersList.vue
@@ -1,15 +1,25 @@
-<template>
-  <h1>OrdersList</h1>
-</template>
-
 <script>
 export default {
   name: 'OrdersList'
 }
 </script>
 
+<template>
+  <div class="orders-list">
+
+  </div>
+</template>
+
 <style scoped>
-h1 {
-  font-size: 5em;
+.orders-list {
+  padding: 20px;
+  cursor: pointer;
+  border: 2px solid #39495c;
+  margin: 6px 0px 18px 0px;
+}
+
+.orders-list:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
