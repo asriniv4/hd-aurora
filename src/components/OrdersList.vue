cfaf0519909dfad5275155ae46e38ff7e9561d2d
Ash Srinivas <asriniv4@its.jnj.com>
2023-01-10 05:39:09 -0800

    Fixed table html

--- src/components/OrdersList.vue
+++ src/components/OrdersList.vue
@@ -27,8 +27,10 @@ onMounted(() => {
           <td>Model Type</td>
           <td>Status</td>
         </tr>
-        <OrderDetail v-for="order in orders" :key="order.id" :order="order" />
       </thead>
+      <tbody>
+        <OrderDetail v-for="order in orders" :key="order.id" :order="order" />
+      </tbody>
     </table>
   </div>
 </template>
