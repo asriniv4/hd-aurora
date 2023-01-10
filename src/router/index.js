a1b1036c4d756f9bb03445aa43efb91fd0466a7c
Ash Srinivas <asriniv4@its.jnj.com>
2023-01-09 23:43:54 -0800

    Added console logging to vue router failures

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, isNavigationFailure } from "vue-router";
 import OrdersList from "../components/OrdersList";
 
 const routes = [
@@ -12,6 +12,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.afterEach((to, from, failure) => {
+  if (isNavigationFailure(failure)) {
+    console.log('failed navigation', failure);
+  }
 });
 
 export default router;
